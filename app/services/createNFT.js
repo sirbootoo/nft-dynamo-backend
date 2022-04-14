d3af6adf32707624adf2ae730b7171efb920912b
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2022-04-14 22:08:53 +0100

    mic hceck

--- app/services/createNFT.js
+++ app/services/createNFT.js
@@ -6,7 +6,10 @@ const preview = async (sesID) => {
         const info = await nftGenerator({sesID});
         console.log(info, "================== info =====================\n\n");
         return info;
-    } catch(err) {}
+    } catch(err) {
+        console.log(err, "------------ Err --------------- \n\n");
+
+    }
 }
 
 const generateNFTs = async (data) => {
