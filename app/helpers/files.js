8f84df5a3226c5a318c5339244549b1e03ae0ae1
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2022-04-24 11:50:45 +0100

    input folder should be deleted on generation completion

--- app/helpers/files.js
+++ app/helpers/files.js
@@ -4,13 +4,15 @@ const path = require('path');
 const deleteFolderOnGeneration = async (sesID) => {
     // directory path
     const outputDir  = path.resolve(__dirname, `../../output/${sesID}`);
+    const inputDir  = path.resolve(__dirname, `../../input/${sesID}`);
     
 
     // delete directory recursively
     try {
         fs.rmdirSync(outputDir, { recursive: true });
+        fs.rmdirSync(inputDir, { recursive: true });
 
-        console.log(`${outputDir} is deleted!`);
+        console.log(`${outputDir} and ${inputDir} have been deleted!`);
     } catch (err) {
         console.error(`Error while deleting ${outputDir}.`);
     }
